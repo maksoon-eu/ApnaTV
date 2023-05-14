abfabbc8f1935eae99c3ac6c0c11a59014d8f29c
maksoon-eu <73942970+maksoon-eu@users.noreply.github.com>
2023-05-15 00:11:22 +0300

    chose film page final

--- src/components/choseFilm/ChoseFilm.js
+++ src/components/choseFilm/ChoseFilm.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import useWatchService from "../../services/WatchService";
@@ -39,12 +41,15 @@ const Film = ({filmId}) => {
 
 const ChoseFilm = () => {
     const [film, setFilm] = useState([]);
+
     const [similarMovies, setSimilarMovies] = useState([]);
+    const [persons, setPersons] = useState([]);
     const [sequelsAndPrequels, setSequelsAndPrequels] = useState([]);
     const [openModal, setOpenModal] = useState(false);
     const [genres, setGenres] = useState();
     const [country, setCountry] = useState();
     const [premiere, setPremiere] = useState();
+
     const {filmId} = useParams()
     const {error, loading, getFilmForId} = useWatchService();
 
@@ -64,8 +69,10 @@ const ChoseFilm = () => {
 
     const onFilmLoaded = (film) => {
         setFilm(film[0])
+
         setSimilarMovies(film[0].similarMovies)
         setSequelsAndPrequels(film[0].sequelsAndPrequels)
+        setPersons(film[0].persons)
         setGenres(film[0].genres.map((item, i) => {
             return i === film[0].genres.length - 1 ? item.name : `${item.name}, `
         }))
@@ -137,6 +144,27 @@ const ChoseFilm = () => {
             document.body.style.top = '';
         }
     }
+
+    const personsList = persons.map(item => {
+        return (
+            <div key={item.id}>
+                <div className="films__item films__item-after">
+                    <div className="films__item-img">
+                        <LazyLoadImage 
+                            key={item.id}
+                            width='100%' height='100%'
+                            placeholderSrc={loadingImg}
+                            effect="blur"
+                            src={item.photo}
+                            alt={item.name}
+                            style={{transition: '.2s linear'}}
+                        />
+                    </div>
+                    <div className="films__item-name">{item.name === '' ? item.enName : item.name}</div>
+                </div>
+            </div>
+        )
+    })
 
     const similarMovieList = similarMovies.map(item => {
         return (
@@ -252,44 +280,114 @@ const ChoseFilm = () => {
                             {film.ratingImdb}
                         </div>
                     </div>
-                    <table className="choseFilm__table">
-                        <caption className="choseFilm__about">О фильме</caption>
-                        <tbody> 
-                            <tr className="choseFilm__row">
-                                <th className="choseFilm__title">Год</th>
-                                <td className="choseFilm__text">{film.year}</td>
-                            </tr>
-                            <tr className="choseFilm__row">
-                                <th className="choseFilm__title">Страна</th>
-                                <td className="choseFilm__text">{country}</td>
-                            </tr>
-                            <tr className="choseFilm__row">
-                                <th className="choseFilm__title">Жанр</th>
-                                <td className="choseFilm__text">{genres}</td>
-                            </tr>
-                            <tr className="choseFilm__row">
-                                <th className="choseFilm__title">Длительность</th>
-                                <td className="choseFilm__text">{film.movieLength} мин / {Math.floor(film.movieLength / 60)} ч {film.movieLength % 60} мин</td>
-                            </tr>
-                            <tr className="choseFilm__row">
-                                <th className="choseFilm__title">Бюджет</th>
-                                <td className="choseFilm__text">{film.budget === 'undefined undefined' ? '...' : film.budget}</td>
-                            </tr>
-                            <tr className="choseFilm__row">
-                                <th className="choseFilm__title">Сборы в мире</th>
-                                <td className="choseFilm__text">{film.fees === 'undefined undefined' ? '...' : film.fees}</td>
-                            </tr>
-                            <tr className="choseFilm__row">
-                                <th className="choseFilm__title">Премьера в мире</th>
-                                <td className="choseFilm__text">{premiere}</td>
-                            </tr>
-                        </tbody>
-                    </table>
+
+                    <div className="choseFilm__tables">
+                        <table className="choseFilm__table">
+                            <caption className="choseFilm__about">О фильме</caption>
+                            <tbody> 
+                                <tr className="choseFilm__row">
+                                    <th className="choseFilm__title">Год</th>
+                                    <td className="choseFilm__text">{film.year}</td>
+                                </tr>
+                                <tr className="choseFilm__row">
+                                    <th className="choseFilm__title">Страна</th>
+                                    <td className="choseFilm__text">{country}</td>
+                                </tr>
+                                <tr className="choseFilm__row">
+                                    <th className="choseFilm__title">Жанр</th>
+                                    <td className="choseFilm__text">{genres}</td>
+                                </tr>
+                                <tr className="choseFilm__row">
+                                    <th className="choseFilm__title">Длительность</th>
+                                    <td className="choseFilm__text">{film.movieLength === null ? '...' : film.movieLength + ' мин / ' + Math.floor(film.movieLength / 60) + ' ч ' + film.movieLength % 60 + ' мин'}</td>
+                                </tr>
+                                <tr className="choseFilm__row">
+                                    <th className="choseFilm__title">Бюджет</th>
+                                    <td className="choseFilm__text">{film.budget === 'undefined undefined' ? '...' : film.budget}</td>
+                                </tr>
+                                <tr className="choseFilm__row">
+                                    <th className="choseFilm__title">Сборы в мире</th>
+                                    <td className="choseFilm__text">{film.fees === 'undefined undefined' ? '...' : film.fees}</td>
+                                </tr>
+                                <tr className="choseFilm__row">
+                                    <th className="choseFilm__title">Премьера в мире</th>
+                                    <td className="choseFilm__text">{premiere}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <table className="choseFilm__table choseFilm__table--top">
+                            <tbody> 
+                                <tr className="choseFilm__row">
+                                    <th className="choseFilm__title">Режиссер</th>
+                                    <td className="choseFilm__text">
+                                        <div className="choseFilm__text-flex">
+                                            {persons.filter(item => item.profession === 'режиссеры').length !== 0 ? persons.filter(item => item.profession === 'режиссеры').map(item => <div className="actors__text" key={item.id}>{item.name}</div>) : '...'}
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr className="choseFilm__row">
+                                    <th className="choseFilm__title">Редактор</th>
+                                    <td className="choseFilm__text">
+                                        <div className="choseFilm__text-flex">
+                                            {persons.filter(item => item.profession === 'редакторы').length !== 0 ? persons.filter(item => item.profession === 'редакторы').map(item => <div className="actors__text" key={item.id}>{item.name}</div>) : '...'}
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr className="choseFilm__row">
+                                    <th className="choseFilm__title">Продюсер</th>
+                                    <td className="choseFilm__text">
+                                        <div className="choseFilm__text-flex">
+                                            {persons.filter(item => item.profession === 'продюсеры').length !== 0 ? persons.filter(item => item.profession === 'продюсеры').map(item => <div className="actors__text" key={item.id}>{item.name}</div>) : '...'}
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr className="choseFilm__row">
+                                    <th className="choseFilm__title">Оператор</th>
+                                    <td className="choseFilm__text">
+                                        <div className="choseFilm__text-flex">
+                                            {persons.filter(item => item.profession === 'операторы').length !== 0 ? persons.filter(item => item.profession === 'операторы').map(item => <div className="actors__text" key={item.id}>{item.name}</div>) : '...'}
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr className="choseFilm__row">
+                                    <th className="choseFilm__title">Композитор</th>
+                                    <td className="choseFilm__text">
+                                        <div className="choseFilm__text-flex">
+                                            {persons.filter(item => item.profession === 'композиторы').length !== 0 ? persons.filter(item => item.profession === 'композиторы').map(item => <div className="actors__text" key={item.id}>{item.name}</div>) : '...'}
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr className="choseFilm__row">
+                                    <th className="choseFilm__title">Художник</th>
+                                    <td className="choseFilm__text">
+                                        <div className="choseFilm__text-flex">
+                                            {persons.filter(item => item.profession === 'художники').length !== 0 ? persons.filter(item => item.profession === 'художники').map(item => <div className="actors__text" key={item.id}>{item.name}</div>) : '...'}
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr className="choseFilm__row">
+                                    <th className="choseFilm__title">Монтажер</th>
+                                    <td className="choseFilm__text">
+                                        <div className="choseFilm__text-flex">
+                                            {persons.filter(item => item.profession === 'монтажеры').length !== 0 ? persons.filter(item => item.profession === 'монтажеры').map(item => <div className="actors__text" key={item.id}>{item.name}</div>) : '...'}
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
 
                     <Film filmId={filmId} key={filmId}/>
+
+                    <div className="choseFilm__slider" style={{display: personsList.length !== 0 ? 'block' : 'none'}}>
+                        <div className="genre__title">Актеры и съемочная группа</div>
+                        <Slider {...settings} className="main__slider genre__slider"> 
+                            {personsList}  
+                        </Slider>
+                    </div>
                     
                     <div className="choseFilm__slider" style={{display: similarMovieList.length !== 0 ? 'block' : 'none'}}>
-                        <div className="genre__title">Похожие фильмы</div>
+                        <div className="genre__title">Похожее</div>
                         <Slider {...settings} className="main__slider genre__slider"> 
                             {similarMovieList}  
                         </Slider>
