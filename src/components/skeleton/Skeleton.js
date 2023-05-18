0c11e43f915abaafb1e670c21d914071bc2b5347
maksoon-eu <73942970+maksoon-eu@users.noreply.github.com>
2023-05-18 18:20:42 +0300

    style

--- src/components/skeleton/Skeleton.js
+++ src/components/skeleton/Skeleton.js
@@ -6,8 +6,8 @@ import './skeleton.scss';
 const Skeleton = () => {
     return (
         <motion.div
-            initial={{ opacity: 0, y: -200 }}
-            animate={{ opacity: 1, y: 0 }}>
+            initial={{ opacity: 0}}
+            animate={{ opacity: 1}}>
             <div className="skeleton skeleton--main">
                 <div className="pulse skeleton__header">
                     <div className="pulse skeleton__title"></div>
